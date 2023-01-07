f8382c1ddc96933b13967a004026d3fc5ee92f13
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-07 12:49:49 -0600

    REST API CRUD - setup

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/products", (req, res) => {
+  res.send("obteniendo productos");
+});
+
+app.post("/products", (req, res) => {
+  res.send("creado productos");
+});
+
+app.put("/products", (req, res) => {
+  res.send("actulizando productos");
+});
+
+app.delete("/products", (req, res) => {
+  res.send("eliminando productos");
+});
+
+app.get("/products/:id", (req, res) => {
+  res.send("obteniendo un producto");
+});
+
+app.listen(3000);
+console.log(`server on port ${3000}`);
