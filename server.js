65b2098e1ce81c773d7bd7d428c3bfff0465c0bc
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-07 13:38:23 -0600

    REST API PUT, DELETE

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 
 const app = express();
 
-const products = [
+let products = [
   {
     id: 1,
     name: "laptop",
@@ -24,23 +24,40 @@ app.post("/products", (req, res) => {
   res.send(newProduct);
 });
 
-app.put("/products", (req, res) => {
-  res.send("actulizando productos");
-});
-
-app.delete("/products", (req, res) => {
-  res.send("eliminando productos");
-});
-
-app.get("/products/:id", (req, res) => {
+app.put("/products/:id", (req, res) => {
+  const newData = req.body;
   const productFound = products.find((p) => p.id == req.params.id);
-
   if (!productFound) {
     return res.status(404).json({
       message: "Product not found",
     });
   }
-  console.log(productFound);
+  products = products.map((p) =>
+    p.id == req.params.id ? { ...p, ...newData } : p
+  );
+  res.json({
+    message: "product updated successfully",
+  });
+});
+
+app.delete("/products/:id", (req, res) => {
+  const productFound = products.find((p) => p.id == req.params.id);
+  if (!productFound) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
+  products = products.filter((p) => p.id != req.params.id);
+  res.sendStatus(204);
+});
+
+app.get("/products/:id", (req, res) => {
+  const productFound = products.find((p) => p.id == req.params.id);
+  if (!productFound) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
   res.json(productFound);
 });
 
